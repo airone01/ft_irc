7e922843c8a2f9a232e18f4b61079f1a7e442b0f
maximart <maxime.martinez96@hotmail.fr>
2025-12-02 18:59:55 +0100

    feat: add some prototype method in hpp. needs to add more and instance it in cpp

--- src/ReplyMessage.cpp
+++ src/ReplyMessage.cpp
@@ -16,162 +16,164 @@ void ReplyMessage::errCannotSendToChannel(const std::string& arg) {
 	std::cout << "404 ERR_CANNOTSENDTOCHAN " << arg << " :Cannot send to channel" << std::endl;
 }
 
-void ReplyMessage::err_TooManyChannels(const std::string& arg) {
+void ReplyMessage::errTooManyChannels(const std::string& arg) {
 	std::cout << "405 ERR_TOOMANYCHANNELS " << arg << " :You have joined too many channels" << std::endl;
 }
 
-void ReplyMessage::err_WasNoSuckNick(const std::string& arg) {
+void ReplyMessage::errWasNoSuckNick(const std::string& arg) {
 	std::cout << "406 ERR_WASNOSUCHNICK " << arg << " :There was no such nickname" << std::endl;
 }
 
-void ReplyMessage::err_TooManyTargets(const std::string& arg) {
+void ReplyMessage::errTooManyTargets(const std::string& arg) {
 	std::cout << "407 ERR_TOOMANYTARGETS " << arg << " :Duplicate recipients. No message delivered" << std::endl;
 }
 
-void ReplyMessage::err_NoOrigin() {
+void ReplyMessage::errNoOrigin() {
 	std::cout << "409 ERR_NOORIGIN :No origin specified" << std::endl;
 }
 
-void ReplyMessage::err_NoRecipient(const std::string& arg) {
+void ReplyMessage::errNoRecipient(const std::string& arg) {
 	std::cout << "411 ERR_NORECIPIENT :No recipient given " << arg << std::endl;
 }
 
-void ReplyMessage::err_NoTextToSend() {
+void ReplyMessage::errNoTextToSend() {
 	std::cout << "412 ERR_NORECIPIENT :No text to send" << std::endl;
 }
 
-void ReplyMessage::err_NoTopLevel(const std::string& arg) {
+void ReplyMessage::errNoTopLevel(const std::string& arg) {
 	std::cout << "413 ERR_NOTOPLEVEL " << arg << " :No toplevel domain specified" << std::endl;
 }
 
-void ReplyMessage::err_WildTopLevel(const std::string& arg) {
+void ReplyMessage::errWildTopLevel(const std::string& arg) {
 	std::cout << "414 ERR_WILDTOPLEVEL " << arg << " :Wildcard in toplevel domain" << std::endl;
 }
 
-void ReplyMessage::err_UnknownCommand(const std::string& arg) {
+void ReplyMessage::errUnknownCommand(const std::string& arg) {
 	std::cout << "421 ERR_UNKNOWNCOMMAND " << arg << " :Unknown command" << std::endl;
 }
 
-void ReplyMessage::err_NoMOTD() {
+void ReplyMessage::errNoMOTD() {
 	std::cout << "422 ERR_NOMOTD :MOTD File is missing" << std::endl;
 }
 
-void ReplyMessage::err_NoAdminInfo(const std::string& arg) {
+void ReplyMessage::errNoAdminInfo(const std::string& arg) {
 	std::cout << "423 ERR_NOADMININFO " << arg << " :No administrative info available" << std::endl;
 }
 
-void ReplyMessage::err_FileError(const std::string& arg, const std::string& arg2) {
+void ReplyMessage::errFileError(const std::string& arg, const std::string& arg2) {
 	std::cout << "424 ERR_FILEERROR :File error doing" << arg << " on " << arg2 << std::endl;
 }
 
-void ReplyMessage::err_NoNickNameGiven() {
+void ReplyMessage::errNoNickNameGiven() {
 	std::cout << "431 ERR_NONICKNAMEGIVEN :No nickname given" << std::endl;
 }
 
-void ReplyMessage::err_ErroneusNuckname(const std::string& arg) {
+void ReplyMessage::errErroneusNuckname(const std::string& arg) {
 	std::cout << "432 ERR_ERRONEUSNICKNAME " << arg << " :Erroneus nickname" << std::endl;
 }
 
-void ReplyMessage::err_NicknameInUse(const std::string& arg) {
+void ReplyMessage::errNicknameInUse(const std::string& arg) {
 	std::cout << "433 ERR_NICKNAMEINUSE " << arg << " :Nickname is already in use" << std::endl;
 }
 
-void ReplyMessage::err_NickCollision(const std::string& arg) {
+void ReplyMessage::errNickCollision(const std::string& arg) {
 	std::cout << "436 ERR_NICKCOLLISION " << arg << " :Nickname collision KILL" << std::endl;
 }
 
-void ReplyMessage::err_UserNotInChannel(const std::string& arg, const std::string& arg2) {
+void ReplyMessage::errUserNotInChannel(const std::string& arg, const std::string& arg2) {
 	std::cout << "441 ERR_USERNOTINCHANNEL " << arg << " " << arg2 << " :They aren't on that channel" << std::endl;
 }
 
-void ReplyMessage::err_NotOnChannel(const std::string& arg) {
+void ReplyMessage::errNotOnChannel(const std::string& arg) {
 	std::cout << "442 ERR_NOTONCHANNEL " << arg << " :You're not on that channel" << std::endl;
 }
 
-void ReplyMessage::err_UserOnChannel(const std::string& arg, const std::string& arg2) {
+void ReplyMessage::errUserOnChannel(const std::string& arg, const std::string& arg2) {
 	std::cout << "443 ERR_USERONCHANNEL " << arg << " " << arg2 << " :is already on channel" << std::endl;
 }
 
-void ReplyMessage::err_NoLogin(const std::string& arg) {
+void ReplyMessage::errNoLogin(const std::string& arg) {
 	std::cout << "444 ERR_NOLOGIN " << arg << " :User not logged in" << std::endl;
 }
 
-void ReplyMessage::err_SummonDisabled() {
+void ReplyMessage::errSummonDisabled() {
 	std::cout << "445 ERR_SUMMONDISABLED :SUMMON has been disabled" << std::endl;
 }
 
-void ReplyMessage::err_UserDisabled() {
+void ReplyMessage::errUserDisabled() {
 	std::cout << "446 ERR_USERSDISABLED :USERS has been disabled" << std::endl;
 }
 
-void ReplyMessage::err_NotRegistered() {
+void ReplyMessage::errNotRegistered() {
 	std::cout << "451 ERR_NOTREGISTERED :You have not registered" << std::endl;
 }
 
-void ReplyMessage::err_NeedMoreParams(const std::string& arg) {
+void ReplyMessage::errNeedMoreParams(const std::string& arg) {
 	std::cout << "461 ERR_NEEDMOREPARAMS " << arg << " :Not enough parameters" << std::endl;
 }
 
-void ReplyMessage::err_AlreadyRegistered() {
+void ReplyMessage::errAlreadyRegistered() {
 	std::cout << "462 ERR_ALREADYREGISTRED :You may not reregister" << std::endl;
 }
 
-void ReplyMessage::err_NoPermForHost() {
+void ReplyMessage::errNoPermForHost() {
 	std::cout << "463 ERR_NOPERMFORHOST :Your host isn't among the privileged" << std::endl;
 }
 
-void ReplyMessage::err_PasswdMismatch() {
+void ReplyMessage::errPasswdMismatch() {
 	std::cout << "464 ERR_PASSWDMISMATCH :Password incorrect" << std::endl;
 }
 
-void ReplyMessage::err_YoureBannedCreep() {
+void ReplyMessage::errYoureBannedCreep() {
 	std::cout << "465 ERR_YOUREBANNEDCREEP :You are banned from this server" << std::endl;
 }
 
-void ReplyMessage::err_KeySet(const std::string& arg) {
+void ReplyMessage::errKeySet(const std::string& arg) {
 	std::cout << "467 ERR_KEYSET " << arg << " :Channel key already set" << std::endl;
 }
 
-void ReplyMessage::err_ChannelIsFull(const std::string& arg) {
+void ReplyMessage::errChannelIsFull(const std::string& arg) {
 	std::cout << "471 ERR_CHANNELISFULL " << arg << " :Cannot join channel (+l)" << std::endl;
 }
 
-void ReplyMessage::err_UnknownMode(const std::string& arg) {
+void ReplyMessage::errUnknownMode(const std::string& arg) {
 	std::cout << "472 ERR_UNKNOWNMODE " << arg << " :is unknown mode char to me" << std::endl;
 }
 
-void ReplyMessage::err_InviteOnlyChan(const std::string& arg) {
+void ReplyMessage::errInviteOnlyChan(const std::string& arg) {
 	std::cout << "473 ERR_INVITEONLYCHAN " << arg << " :Cannot join channel (+i)" << std::endl;
 }
 
-void ReplyMessage::err_BannedFromChan(const std::string& arg) {
+void ReplyMessage::errBannedFromChan(const std::string& arg) {
 	std::cout << "474 ERR_BANNEDFROMCHAN " << arg << " :Cannot join channel (+b)" << std::endl;
 }
 
-void ReplyMessage::err_BadChannelKey(const std::string& arg) {
+void ReplyMessage::errBadChannelKey(const std::string& arg) {
 	std::cout << "475 ERR_BADCHANNELKEY " << arg << " :Cannot join channel (+k)" << std::endl;
 }
 
-void ReplyMessage::err_NoPrivileges() {
+void ReplyMessage::errNoPrivileges() {
 	std::cout << "481 ERR_NOPRIVILEGES :Permission Denied- You're not an IRC operator" << std::endl;
 }
 
-void ReplyMessage::err_ChaNoPrivsNeeded(const std::string& arg) {
+void ReplyMessage::errChaNoPrivsNeeded(const std::string& arg) {
 	std::cout << "482 ERR_CHANOPRIVSNEEDED " << arg << " :You're not channel operator" << std::endl;
 }
 
-void ReplyMessage::err_CantKillServer() {
+void ReplyMessage::errCantKillServer() {
 	std::cout << "483 ERR_CANTKILLSERVER :You cant kill a server!" << std::endl;
 }
 
-void ReplyMessage::err_NoOPerHost() {
+void ReplyMessage::errNoOPerHost() {
 	std::cout << "491 ERR_NOOPERHOST :No O-lines for your host" << std::endl;
 }
 
-void ReplyMessage::err_UModeUnknownFlag() {
+void ReplyMessage::errUModeUnknownFlag() {
 	std::cout << "501 ERR_UMODEUNKNOWNFLAG :Unknown MODE flag" << std::endl;
 }
 
-void ReplyMessage::err_UserDontMatch() {
+void ReplyMessage::errUserDontMatch() {
 	std::cout << "502 ERR_USERSDONTMATCH :Cant change mode for other users" << std::endl;
 }
+
+//TODO RPLMSG
--- src/ReplyMessage.hpp
+++ src/ReplyMessage.hpp
@@ -9,203 +9,233 @@ public:
 	static void errNoSuchServer(const std::string& arg); // "<server name> :No such server" 402
 	static void errNoSuckChannel(const std::string& arg); // "<channel name> :No such channel" 403
 	static void errCannotSendToChannel(const std::string& arg); // "<channel name> :Cannot send to channel" 404
-	static void err_TooManyChannels(const std::string& arg); // "<channel name> :You have joined too many channels" 405
-	static void err_WasNoSuckNick(const std::string& arg); // "<nickname> :There was no such nickname" 406
-	static void err_TooManyTargets(const std::string& arg); // "<target> :Duplicate recipients. No message delivered" 407
-	static void err_NoOrigin(); // ":No origin specified" 409
-	static void err_NoRecipient(const std::string& arg); // ":No recipient given (<command>)" 411
-	static void err_NoTextToSend(); // ":No text to send" 412
-	static void err_NoTopLevel(const std::string& arg); // "<mask> :No toplevel domain specified" 413
-	static void err_WildTopLevel(const std::string& arg); // "<mask> :Wildcard in toplevel domain" 414
-	static void err_UnknownCommand(const std::string& arg); // "<command> :Unknown command" 421
-	static void err_NoMOTD(); // ":MOTD File is missing" 422
-	static void err_NoAdminInfo(const std::string& arg); // "<server> :No administrative info available" 423
-	static void err_FileError(const std::string& arg, const std::string& arg2); // ":File error doing <file op> on <file>" 424
-	static void err_NoNickNameGiven(); //  ":No nickname given" 431
-	static void err_ErroneusNuckname(const std::string& arg); //  "<nick> :Erroneus nickname" 432
-	static void err_NicknameInUse(const std::string& arg); //  "<nick> :Nickname is already in use" 433
-	static void err_NickCollision(const std::string& arg); //  "<nick> :Nickname collision KILL" 434
-	static void err_UserNotInChannel(const std::string& arg, const std::string& arg2); //  "<nick> <channel> :They aren't on that channel" 441
-	static void err_NotOnChannel(const std::string& arg); //  ERR_NOTONCHANNEL"<channel> :You're not on that channel" 442
-	static void err_UserOnChannel(const std::string& arg, const std::string& arg2); //  "<user> <channel> :is already on channel" 443
-	static void err_NoLogin(const std::string& arg); //   "<user> :User not logged in" 444
-	static void err_SummonDisabled(); //  ":SUMMON has been disabled" 445
-	static void err_UserDisabled(); //  ":USERS has been disabled"  446
-	static void err_NotRegistered(); //  ":You have not registered" 451
-	static void err_NeedMoreParams(const std::string& arg); //  "<command> :Not enough parameters" 461
-	static void err_AlreadyRegistered(); //  ":You may not reregister" 462
-	static void err_NoPermForHost(); //  ":Your host isn't among the privileged" 463
-	static void err_PasswdMismatch(); //  ":Password incorrect" 464
-	static void err_YoureBannedCreep(); //  ":You are banned from this server" 465
-	static void err_KeySet(const std::string& arg); //  "<channel> :Channel key already set" 467
-	static void err_ChannelIsFull(const std::string& arg); //  "<channel> :Cannot join channel (+l)" 471
-	static void err_UnknownMode(const std::string& arg); //  "<char> :is unknown mode char to me" 472
-	static void err_InviteOnlyChan(const std::string& arg); //  "<channel> :Cannot join channel (+i)" 473
-	static void err_BannedFromChan(const std::string& arg); //  "<channel> :Cannot join channel (+b)" 474
-	static void err_BadChannelKey(const std::string& arg); //  "<channel> :Cannot join channel (+k)" 475
-	static void err_NoPrivileges(); //  ":Permission Denied- You're not an IRC operator" 481
-	static void err_ChaNoPrivsNeeded(const std::string& arg); //  "<channel> :You're not channel operator" 482
-	static void err_CantKillServer(); //  ":You cant kill a server!" 483
-	static void err_NoOPerHost(); // ":No O-lines for your host" 491
-	static void err_UModeUnknownFlag(); // ":Unknown MODE flag" 501
-	static void err_UserDontMatch(); //  ":Cant change mode for other users" 502
+	static void errTooManyChannels(const std::string& arg); // "<channel name> :You have joined too many channels" 405
+	static void errWasNoSuckNick(const std::string& arg); // "<nickname> :There was no such nickname" 406
+	static void errTooManyTargets(const std::string& arg); // "<target> :Duplicate recipients. No message delivered" 407
+	static void errNoOrigin(); // ":No origin specified" 409
+	static void errNoRecipient(const std::string& arg); // ":No recipient given (<command>)" 411
+	static void errNoTextToSend(); // ":No text to send" 412
+	static void errNoTopLevel(const std::string& arg); // "<mask> :No toplevel domain specified" 413
+	static void errWildTopLevel(const std::string& arg); // "<mask> :Wildcard in toplevel domain" 414
+	static void errUnknownCommand(const std::string& arg); // "<command> :Unknown command" 421
+	static void errNoMOTD(); // ":MOTD File is missing" 422
+	static void errNoAdminInfo(const std::string& arg); // "<server> :No administrative info available" 423
+	static void errFileError(const std::string& arg, const std::string& arg2); // ":File error doing <file op> on <file>" 424
+	static void errNoNickNameGiven(); //  ":No nickname given" 431
+	static void errErroneusNuckname(const std::string& arg); //  "<nick> :Erroneus nickname" 432
+	static void errNicknameInUse(const std::string& arg); //  "<nick> :Nickname is already in use" 433
+	static void errNickCollision(const std::string& arg); //  "<nick> :Nickname collision KILL" 434
+	static void errUserNotInChannel(const std::string& arg, const std::string& arg2); //  "<nick> <channel> :They aren't on that channel" 441
+	static void errNotOnChannel(const std::string& arg); // "<channel> :You're not on that channel" 442
+	static void errUserOnChannel(const std::string& arg, const std::string& arg2); // "<user> <channel> :is already on channel" 443
+	static void errNoLogin(const std::string& arg); // "<user> :User not logged in" 444
+	static void errSummonDisabled(); // ":SUMMON has been disabled" 445
+	static void errUserDisabled(); //  ":USERS has been disabled"  446
+	static void errNotRegistered(); //  ":You have not registered" 451
+	static void errNeedMoreParams(const std::string& arg); //  "<command> :Not enough parameters" 461
+	static void errAlreadyRegistered(); //  ":You may not reregister" 462
+	static void errNoPermForHost(); //  ":Your host isn't among the privileged" 463
+	static void errPasswdMismatch(); //  ":Password incorrect" 464
+	static void errYoureBannedCreep(); //  ":You are banned from this server" 465
+	static void errKeySet(const std::string& arg); //  "<channel> :Channel key already set" 467
+	static void errChannelIsFull(const std::string& arg); //  "<channel> :Cannot join channel (+l)" 471
+	static void errUnknownMode(const std::string& arg); //  "<char> :is unknown mode char to me" 472
+	static void errInviteOnlyChan(const std::string& arg); //  "<channel> :Cannot join channel (+i)" 473
+	static void errBannedFromChan(const std::string& arg); //  "<channel> :Cannot join channel (+b)" 474
+	static void errBadChannelKey(const std::string& arg); //  "<channel> :Cannot join channel (+k)" 475
+	static void errNoPrivileges(); //  ":Permission Denied- You're not an IRC operator" 481
+	static void errChaNoPrivsNeeded(const std::string& arg); //  "<channel> :You're not channel operator" 482
+	static void errCantKillServer(); //  ":You cant kill a server!" 483
+	static void errNoOPerHost(); // ":No O-lines for your host" 491
+	static void errUModeUnknownFlag(); // ":Unknown MODE flag" 501
+	static void errUserDontMatch(); //  ":Cant change mode for other users" 502
 
-	//        300     RPL_NONE
-	//                        Dummy reply number. Not used.
-	//        302     RPL_USERHOST
-	//                        ":[<reply>{<space><reply>}]"
-	//        303     RPL_ISON
-	//                        ":[<nick> {<space><nick>}]"
-	//        301     RPL_AWAY
-	//                        "<nick> :<away message>"
-	//        305     RPL_UNAWAY
-	//                        ":You are no longer marked as being away"
-	//        306     RPL_NOWAWAY
-	//                        ":You have been marked as being away"
-	//        311     RPL_WHOISUSER
-	//                        "<nick> <user> <host> * :<real name>"
-	//        312     RPL_WHOISSERVER
-	//                        "<nick> <server> :<server info>"
-	//        313     RPL_WHOISOPERATOR
-	//                        "<nick> :is an IRC operator"
-	//        317     RPL_WHOISIDLE
-	//                        "<nick> <integer> :seconds idle"
-	//        318     RPL_ENDOFWHOIS
-	//                        "<nick> :End of /WHOIS list"
-	//        319     RPL_WHOISCHANNELS
-	//                        "<nick> :{[@|+]<channel><space>}"
-	//        314     RPL_WHOWASUSER
-	//                        "<nick> <user> <host> * :<real name>"
-	//        369     RPL_ENDOFWHOWAS
-	//                        "<nick> :End of WHOWAS"
-	//        321     RPL_LISTSTART
-	//                        "Channel :Users  Name"
-	//        322     RPL_LIST
-	//                        "<channel> <# visible> :<topic>"
-	//        323     RPL_LISTEND
-	//                        ":End of /LIST"
-	//        324     RPL_CHANNELMODEIS
-	//                        "<channel> <mode> <mode params>"
-	//        331     RPL_NOTOPIC
-	//                        "<channel> :No topic is set"
-	//        332     RPL_TOPIC
-	//                        "<channel> :<topic>"
-	//        341     RPL_INVITING
-	//                        "<channel> <nick>"
-	//        342     RPL_SUMMONING
-	//                        "<user> :Summoning user to IRC"
-	//        351     RPL_VERSION
-	//                        "<version>.<debuglevel> <server> :<comments>"
+	static void rplNone(const std::string& arg); // Dummy reply number. Not used. 300
+	static void rplUserHost(const std::string& arg); // ":[<reply>{<space><reply>}]" 302
+	static void rplIsOn(const std::string& arg); // ":[<nick> {<space><nick>}]" 303
+	static void rplAway(const std::string& arg); // "<nick> :<away message>" 301
+	static void rplUnAway(const std::string& arg); // ":You are no longer marked as being away" 305
+	static void rplNoAway(const std::string& arg); // ":You have been marked as being away" 306
+	static void rplWhoIsUser(const std::string& arg); // "<nick> <user> <host> * :<real name>" 311
+	static void rplWhoIsServer(const std::string& arg); // "<nick> <server> :<server info>" 312
+	static void rplWhoIsOperator(const std::string& arg); // "<nick> :is an IRC operator" 313
+	static void rplWhoIsIdle(const std::string& arg); // "<nick> <integer> :seconds idle" 317
+	static void rplEndOfWhoIs(const std::string& arg); // "<nick> :End of /WHOIS list" 318
+	static void rplWhoIsChannels(const std::string& arg); // "<nick> :{[@|+]<channel><space>}" 319
+	static void rplWhoAsUser(const std::string& arg); // "<nick> <user> <host> * :<real name>" 314
+	static void rplEndOfWhoWas(const std::string& arg); // "<nick> :End of WHOWAS" 369
+	static void rplListStart(const std::string& arg); // "Channel :Users  Name" 321
+	static void rplList(const std::string& arg); // "<channel> <# visible> :<topic>" 322
+	static void rplListEnd(const std::string& arg); // ":End of /LIST" 323
+	static void rplChannelModeIs(const std::string& arg); // "<channel> <mode> <mode params>" 324
+	static void rplNoTopic(const std::string& arg); // "<channel> :No topic is set" 331
+	static void rplTopic(const std::string& arg); // "<channel> :<topic>" 332
+	static void rplInviting(const std::string& arg); // "<channel> <nick>" 341
+	static void rplSummoning(const std::string& arg); // "<user> :Summoning user to IRC" 342
+	static void rplVersion(const std::string& arg); // "<version>.<debuglevel> <server> :<comments>" 351
+	/TODO
+	static void rpl(const std::string& arg); //
 	//        352     RPL_WHOREPLY
 	//                        "<channel> <user> <host> <server> <nick> \
 	//                         <H|G>[*][@|+] :<hopcount> <real name>"
+	static void rpl(const std::string& arg); //
 	//        315     RPL_ENDOFWHO
 	//                        "<name> :End of /WHO list"
+	static void rpl(const std::string& arg); //
 	//        353     RPL_NAMREPLY
 	//                        "<channel> :[[@|+]<nick> [[@|+]<nick> [...]]]"
+	static void rpl(const std::string& arg); //
 	//        366     RPL_ENDOFNAMES
 	//                        "<channel> :End of /NAMES list"
+	static void rpl(const std::string& arg); //
 	//        364     RPL_LINKS
 	//                        "<mask> <server> :<hopcount> <server info>"
+	static void rpl(const std::string& arg); //
 	//        365     RPL_ENDOFLINKS
 	//                        "<mask> :End of /LINKS list"
+	static void rpl(const std::string& arg); //
 	//        367     RPL_BANLIST
 	//                        "<channel> <banid>"
+	static void rpl(const std::string& arg); //
 	//        368     RPL_ENDOFBANLIST
 	//                        "<channel> :End of channel ban list"
+	static void rpl(const std::string& arg); //
 	//        371     RPL_INFO
 	//                        ":<string>"
+	static void rpl(const std::string& arg); //
 	//        374     RPL_ENDOFINFO
 	//                        ":End of /INFO list"
+	static void rpl(const std::string& arg); //
 	//        375     RPL_MOTDSTART
 	//                        ":- <server> Message of the day - "
+	static void rpl(const std::string& arg); //
 	//        372     RPL_MOTD
 	//                        ":- <text>"
+	static void rpl(const std::string& arg); //
 	//        376     RPL_ENDOFMOTD
 	//                        ":End of /MOTD command"
+	static void rpl(const std::string& arg); //
 	//        381     RPL_YOUREOPER
 	//                        ":You are now an IRC operator"
+	static void rpl(const std::string& arg); //
 	//        382     RPL_REHASHING
 	//                        "<config file> :Rehashing"
+	static void rpl(const std::string& arg); //
 	//        391     RPL_TIME
 	//                        "<server> :<string showing server's local time>"
+	static void rpl(const std::string& arg); //
 	//        392     RPL_USERSSTART
 	//                        ":UserID   Terminal  Host"
+	static void rpl(const std::string& arg); //
 	//        393     RPL_USERS
 	//                        ":%-8s %-9s %-8s"
+	static void rpl(const std::string& arg); //
 	//        394     RPL_ENDOFUSERS
 	//                        ":End of users"
+	static void rpl(const std::string& arg); //
 	//        395     RPL_NOUSERS
 	//                        ":Nobody logged in"
+	static void rpl(const std::string& arg); //
 	//        200     RPL_TRACELINK
 	//                        "Link <version & debug level> <destination> \
 	//                         <next server>"
+	static void rpl(const std::string& arg); //
 	//        201     RPL_TRACECONNECTING
 	//                        "Try. <class> <server>"
+	static void rpl(const std::string& arg); //
 	//        202     RPL_TRACEHANDSHAKE
 	//                        "H.S. <class> <server>"
+	static void rpl(const std::string& arg); //
 	//        203     RPL_TRACEUNKNOWN
 	//                        "???? <class> [<client IP address in dot form>]"
+	static void rpl(const std::string& arg); //
 	//        204     RPL_TRACEOPERATOR
 	//                        "Oper <class> <nick>"
+	static void rpl(const std::string& arg); //
 	//        205     RPL_TRACEUSER
 	//                        "User <class> <nick>"
+	static void rpl(const std::string& arg); //
 	//        206     RPL_TRACESERVER
 	//                        "Serv <class> <int>S <int>C <server> \
 	//                         <nick!user|*!*>@<host|server>"
+	static void rpl(const std::string& arg); //
 	//        208     RPL_TRACENEWTYPE
 	//                        "<newtype> 0 <client name>"
+	static void rpl(const std::string& arg); //
 	//        261     RPL_TRACELOG
 	//                        "File <logfile> <debug level>"
+	static void rpl(const std::string& arg); //
 	//        211     RPL_STATSLINKINFO
 	//                        "<linkname> <sendq> <sent messages> \
 	//                         <sent bytes> <received messages> \
 	//                         <received bytes> <time open>"
+	static void rpl(const std::string& arg); //
 	//        212     RPL_STATSCOMMANDS
 	//                        "<command> <count>"
+	static void rpl(const std::string& arg); //
 	//        213     RPL_STATSCLINE
 	//                        "C <host> * <name> <port> <class>"
+	static void rpl(const std::string& arg); //
 	//        214     RPL_STATSNLINE
 	//                        "N <host> * <name> <port> <class>"
+	static void rpl(const std::string& arg); //
 	//        215     RPL_STATSILINE
 	//                        "I <host> * <host> <port> <class>"
+	static void rpl(const std::string& arg); //
 	//        216     RPL_STATSKLINE
 	//                        "K <host> * <username> <port> <class>"
+	static void rpl(const std::string& arg); //
 	//        218     RPL_STATSYLINE
 	//                        "Y <class> <ping frequency> <connect \
 	//                         frequency> <max sendq>"
+	static void rpl(const std::string& arg); //
 	//        219     RPL_ENDOFSTATS
 	//                        "<stats letter> :End of /STATS report"
+	static void rpl(const std::string& arg); //
 	//        241     RPL_STATSLLINE
 	//                        "L <hostmask> * <servername> <maxdepth>"
+	static void rpl(const std::string& arg); //
 	//        242     RPL_STATSUPTIME
 	//                        ":Server Up %d days %d:%02d:%02d"
+	static void rpl(const std::string& arg); //
 	//        243     RPL_STATSOLINE
 	//                        "O <hostmask> * <name>"
+	static void rpl(const std::string& arg); //
 	//        244     RPL_STATSHLINE
 	//                        "H <hostmask> * <servername>"
+	static void rpl(const std::string& arg); //
 	//        221     RPL_UMODEIS
 	//                        "<user mode string>"
+	static void rpl(const std::string& arg); //
 	//        251     RPL_LUSERCLIENT
 	//                        ":There are <integer> users and <integer> \
 	//                         invisible on <integer> servers"
+	static void rpl(const std::string& arg); //
 	//        252     RPL_LUSEROP
 	//                        "<integer> :operator(s) online"
+	static void rpl(const std::string& arg); //
 	//        253     RPL_LUSERUNKNOWN
 	//                        "<integer> :unknown connection(s)"
+	static void rpl(const std::string& arg); //
 	//        254     RPL_LUSERCHANNELS
 	//                        "<integer> :channels formed"
+	static void rpl(const std::string& arg); //
 	//        255     RPL_LUSERME
 	//                        ":I have <integer> clients and <integer> \
 	//                          servers"
+	static void rpl(const std::string& arg); //
 	//        256     RPL_ADMINME
 	//                        "<server> :Administrative info"
+	static void rpl(const std::string& arg); //
 	//        257     RPL_ADMINLOC1
 	//                        ":<admin info>"
+	static void rpl(const std::string& arg); //
 	//        258     RPL_ADMINLOC2
 	//                        ":<admin info>"
+	static void rpl(const std::string& arg); //
 	//        259     RPL_ADMINEMAIL
 	//                        ":<admin info>"
+	static void rpl(const std::string& arg); //
 };
 
 #endif
